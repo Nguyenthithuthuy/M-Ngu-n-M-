5412a5cae91e62f6e8f63bbbba264bbdfd1073a9
hangatthuyhamMNM <ngthithuthuy96@gmail.com>
2017-07-16 08:47:01 +0700

    Mã Nguồn: Quản lí bán hàng
    Gồm các form : Đăng nhập, nhập nguyên liệu, loại món, quản lí bàn, hóa đơn

---
+++ DangNhap.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterQuan.master" AutoEventWireup="true" CodeFile="DangNhap.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder" Runat="Server"> 
+    
+    <table style="width: 100%; height: 179px;">
+        <tr>
+            <td colspan="3" style="text-align: center">ĐĂNG NHẬP VÀO HỆ THỐNG </td>
+        </tr>
+        <tr>
+            <td style="width: 192px; height: 67px">Tên Đăng Nhập </td>
+            <td style="text-align: center; width: 446px; height: 67px">
+                <asp:TextBox ID="txtTenDN" runat="server" Width="246px"></asp:TextBox>
+            </td>
+            <td style="height: 67px">
+                <asp:Button ID="bntDN" runat="server" OnClick="bntDN_Click" Text="OK" />
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 192px">Mật Khẩu </td>
+            <td style="width: 446px; text-align: center">
+                <asp:TextBox ID="txtMatKhau" runat="server" Width="246px"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Button ID="bntThoat" runat="server" OnClick="bntThoat_Click" Text="THOÁT" />
+            </td>
+        </tr>
+    </table>
+    
+</asp:Content>
+
---
+++ DangNhap.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    String cn = @"Provider=IBMOLEDB.DB2COPY1;Data Source=QCF;Password=251096thuy1;User ID=NK;Location=127.0.0.1";
+
+    bool KiemTraDN(string TenDN, string MatKhau)
+    {
+        OleDbConnection dbCon = new OleDbConnection(cn);
+        dbCon.Open();
+        string sql = ("SELECT TENDN FROM user1.NHANVIEN WHERE TENDN ='" + TenDN + "' AND  MATKHAU = '" + MatKhau + "'");
+        OleDbCommand cmd = new OleDbCommand(sql, dbCon);
+        OleDbDataReader reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+            return true;
+        else
+            return false;
+    }
+    protected void bntDN_Click(object sender, EventArgs e)
+    {
+
+        if (KiemTraDN(txtTenDN.Text, txtMatKhau.Text) == true)
+        {
+            Session["TENDN"] = txtTenDN.Text;
+            Response.Redirect("ndTrangChinh.aspx");
+        }
+        else
+        {
+            Session["TENDN"] = "";
+            Response.Redirect("QuangCao.aspx");
+        }
+
+
+    
+}
+
+    protected void bntThoat_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("QuangCao.aspx");
+    }
+}
